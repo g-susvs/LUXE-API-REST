4c94e9cd73839da08ddb26e58ad793b18a992672
Jesus Valdivia <jesusjesusvaldivia@hotmail.com>
2022-09-30 18:32:00 -0500

    Container and Items rutas y controllador

--- controllers/container.js
+++ controllers/container.js
@@ -1,39 +1,72 @@
+const { response} = require('express');
+const Container = require('../models/container')
 
-const getContainers = (req,res) => {
-    res.status(200).json({
-        msg:'get - containers'
-    })
-}
-const getContainer = (req,res) => {
-    const {id} = req.params;
-    res.status(200).json({
-        msg:'get - container ',
-        id
-    })
-}
-const createContainer = (req,res) => {
-    const body = req.body;
-    res.status(200).json({
-        msg:'post - container ',
-        body
-    })
-}
-const updateContainer = (req,res) => {
-    const {id} = req.params;
-    const body = req.body;
-    res.status(200).json({
-        msg:'update - container',
-        id,
-        body
-    })
-}
-const deleteContainer = (req,res) => {
-    const {id} = req.params;
+const getContainers = async(req,res) => {
     
-    res.status(200).json({
-        msg:'delete - container',
-        id
-    })
+    const query = { estado: true };
+
+    try {
+        const [containers, total] = await Promise.all([
+            Container.find(query),
+            Container.countDocuments(query)
+        ])
+        res.status(200).json({ total, containers })
+        
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
+
+}
+const getContainer = async(req,res= response) => {
+    
+    const {id} = req.params;
+    const container = await Container.findById(id)
+                            .populate('user','name');
+    res.json(container);
+}
+const createContainer = async(req,res= response) => {
+    
+    const name= req.body.name.toUpperCase();
+    const type_container = req.body.type_container.toUpperCase();
+
+
+    const containerDB = await Container.findOne({name,type_container});
+
+    if (containerDB) {
+        return res.status(400).json({
+            msg: `El container ${containerDB.name}, ya existe`
+        })
+    }
+
+    const data ={
+        name,
+        type_container,
+        user: req.user._id,
+    }
+
+    const container = new Container(data);
+
+    await container.save();
+
+    res.status(201).json(container);
+}
+const updateContainer = async(req,res) => {
+    const {id} = req.params;
+    const {user, ...data} = req.body;
+
+    const container = await Container.findByIdAndUpdate(id, data, {new:true});
+
+    res.json(container);
+}
+const deleteContainer = async(req,res) => {
+    
+    const {id} = req.params;
+
+    const container = await Container.findByIdAndUpdate(id,{estado:false},{new:true});
+    
+
+    res.json(container)
 }
 
 module.exports = {
--- controllers/item.js
+++ controllers/item.js
@@ -1,9 +1,94 @@
-const getItems = (req,res) => {
-    res.status(200).json({
-        msg:'get - items'
+const { response } = require("express");
+const Item = require("../models/item");
+
+const getItems = async(req,res= response) => {
+
+    const {limite = 0 , desde = 0} = req.query;
+    const query = {estado:true};
+
+    const [total, items] = await Promise.all([
+        Item.countDocuments(query),
+        Item.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+        .populate("user","name")
+        .populate("container","name")
+    ])
+    res.json({
+        total,
+        items,
     })
 }
 
+const getItem = async(req,res=response) => {
+
+    const {id} = req.params;
+    const item = await Item.findById(id)
+                            .populate('user','name')
+                            .populate('container','name');
+
+    res.json(item);
+}
+
+const postItems = async(req,res=response) => {
+    
+    const {estado, user, ...body} = req.body;
+
+    const itemDB = await Item.findOne({name: body.name});
+
+    if (itemDB) {
+        return res.status(400).json({
+            msg: `El Item ${itemDB.name}, ya existe`
+        });
+    }
+
+    //geenrar la data a guardar
+    const data={
+        ...body,
+        name : body.name.toUpperCase(),
+        user: req.user._id
+    }
+
+    const item = new Item(data);
+
+    //Guardar en DB
+    await item.save();
+
+    res.status(201).json(item);
+}
+
+const putItems = async(req,res=response) => {
+
+    const{id}= req.params;
+    const{ estado, user, ...data} =req.body;
+
+    if (data.name) {
+        data.name = data.name.toUpperCase();
+    }
+
+    data.user = req.user._id;
+
+    const item = await Item.findByIdAndUpdate(id, data, {new:true});
+
+    res.json({
+        item
+    })
+}
+
+const deleteItems = async(req,res=response) => {
+
+    const {id} = req.params;
+
+    const itemBorrado = await Item.findByIdAndUpdate(id,{estado:false},{new:true});
+    // const categoriaAutenticado = req.usuario;
+
+    res.json(itemBorrado)
+}
+
 module.exports={
-    getItems
+    getItems,
+    postItems,
+    putItems,
+    deleteItems,
+    getItem
 }
