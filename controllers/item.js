af5f9e115636acfb932484ee4f1d03f90b3d9435
g-susvs <jesus.savs19@gmail.com>
2022-12-05 22:13:25 -0500

    Eliminar imagen de cloudinary al eliminar un item

--- controllers/item.js
+++ controllers/item.js
@@ -1,20 +1,23 @@
 const { response } = require("express");
 const { Container } = require("../models");
+const cloudinary = require('cloudinary').v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const Item = require("../models/item");
 
-const getItems = async(req,res= response) => {
+const getItems = async (req, res = response) => {
 
-    const {limite = 0 , desde = 0} = req.query;
-    const query = {state:true};
+    const { limite = 0, desde = 0 } = req.query;
+    const query = { state: true };
 
     try {
         const [total, items] = await Promise.all([
             Item.countDocuments(query),
             Item.find(query)
-            .skip(Number(desde))
-            .limit(Number(limite))
-            .populate("user",["name","email"])
-            .populate("container",["name","name_by_user"])
+                .skip(Number(desde))
+                .limit(Number(limite))
+                .populate("user", ["name", "email"])
+                .populate("container", ["name", "name_by_user"])
         ])
         res.json({
             total,
@@ -27,57 +30,48 @@ const getItems = async(req,res= response) => {
 
 }
 
-const getItem = async(req,res=response) => {
+const getItem = async (req, res = response) => {
 
-    const {id} = req.params;
+    const { id } = req.params;
 
     try {
         const item = await Item.findById(id) // evaluar el state de y demas
-                                .populate('user','name')
-                                .populate('container','name');
-    
+            .populate('user', 'name')
+            .populate('container', 'name');
+
         res.json(item);
-        
+
     } catch (error) {
         console.log(error);
         res.json(error);
     }
 }
 
-const postItems = async(req,res=response) => {
-    
-    const {id} = req.user;
+const postItems = async (req, res = response) => {
+
+    const { id } = req.user;
     const { container } = req.body;
 
     const containerExist = await Container.findById(container);
 
-    if(!containerExist){
+    if (!containerExist) {
         return res.status(400).json({
             msg: `No existe contenedor con el id ${container}`
         })
     }
 
-    if( containerExist.assign_user != id ){
+    if (containerExist.assign_user != id) {
         return res.status(403).json({
             msg: `No tiene acceso al contenedor: ${containerExist.name}`
         })
     }
 
-
-    const {state, user, ...body} = req.body;
-
-    const itemDB = await Item.findOne({name: body.name});
-
-    if (itemDB) {
-        return res.status(400).json({
-            msg: `El Item ${itemDB.name}, ya existe`
-        });
-    }
+    const { state, user, ...body } = req.body;
 
     //geenrar la data a guardar
-    const data={
+    const data = {
         ...body,
-        name : body.name.toUpperCase(),
+        name: body.name,
         user: req.user._id
     }
 
@@ -93,10 +87,10 @@ const postItems = async(req,res=response) => {
     res.status(201).json(item);
 }
 
-const putItems = async(req,res=response) => {
+const putItems = async (req, res = response) => {
 
-    const{id}= req.params;
-    const{ state, user, ...data} =req.body;
+    const { id } = req.params;
+    const { state, user, ...data } = req.body;
 
     if (data.name) {
         data.name = data.name.toUpperCase();
@@ -104,45 +98,61 @@ const putItems = async(req,res=response) => {
 
     data.user = req.user._id;
 
-    const item = await Item.findByIdAndUpdate(id, data, {new:true});
+    const item = await Item.findByIdAndUpdate(id, data, { new: true });
 
     res.json({
         item
     })
 }
 
-const deleteItems = async(req,res=response) => {
+const deleteItems = async (req, res = response) => {
 
-    const {id} = req.params;
+    const { id } = req.params;
 
-    const itemBorrado = await Item.findByIdAndUpdate(id,{state:false},{new:true});
-
-    // decrementar el nro de items 
-    const container = await Container.findById(itemBorrado.container);
-    container.nro_items = container.nro_items - 1;
-    await container.save();
-    
-    res.json({
-        msg: "Item eliminado",
-        itemBorrado})
-}
-const deleteItemDB = async(req,res=response) => {
-
-    const {id} = req.params;
-
-    const itemBorrado = await Item.findByIdAndDelete(id,{new:true})
+    const itemBorrado = await Item.findByIdAndUpdate(id, { state: false }, { new: true });
 
     // decrementar el nro de items 
     const container = await Container.findById(itemBorrado.container);
     container.nro_items = container.nro_items - 1;
     await container.save();
-    
+
     res.json({
         msg: "Item eliminado",
-        itemBorrado})
+        itemBorrado
+    })
+}
+const deleteItemDB = async (req, res = response) => {
+
+    const { id } = req.params;
+
+    const itemBorrado = await Item.findByIdAndDelete(id, { new: true })
+
+    // borrar las imagenes de cloudinary
+    if (itemBorrado.img_client) {
+        const nameArr = itemBorrado.img_client.split("/");
+        const name = nameArr[nameArr.length - 1];
+        const [public_id] = name.split(".");
+        await cloudinary.uploader.destroy(`LUXE/items/${public_id}`);
+    }
+    if (itemBorrado.img_store) {
+        const nameArr = itemBorrado.img_store.split("/");
+        const name = nameArr[nameArr.length - 1];
+        const [public_id] = name.split(".");
+        await cloudinary.uploader.destroy(`LUXE/items/${public_id}`);
+    }
+
+    // decrementar el nro de items 
+    const container = await Container.findById(itemBorrado.container);
+    container.nro_items = container.nro_items - 1;
+    await container.save();
+
+    res.json({
+        msg: "Item eliminado",
+        itemBorrado
+    })
 }
 
-module.exports={
+module.exports = {
     getItems,
     postItems,
     putItems,
